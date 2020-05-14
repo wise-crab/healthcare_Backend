cb55057809398c39b82fa8a9fc57a81fb9baedde
luis_parra <luis.parra.pulido@gmail.com>
2020-05-14 14:57:25 -0500

    [DOCUMENTATION] add swagger

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+
+const swaggerUi = require('swagger-ui-express');
+
 const config = require('../config');
 
 const app = express();
 
 const user = require('./components/user/network');
 
+const swaggerDoc = require('./swagger.json');
+
+//Routes
 app.use(user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   process.stdout.write(`App listening at ${config.api.host}:${config.api.port}`);
